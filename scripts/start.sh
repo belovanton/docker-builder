0e2a38598ddd36622305d6f66b6086a9bc2bc0bd
Anton Belov <belov_ab@kmplzt.de>
2015-12-15 14:23:36 +0300

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+#!/bin/bash
+
+if [ -n "${SSH_PASSWORD}" ]; then
+    echo "root:${SSH_PASSWORD}" | chpasswd
+fi
+if [ -n "${SSH_PASSWORD_WWW}" ]; then
+    echo "www-data:${SSH_PASSWORD_WWW}" | chpasswd
+    sed -i 's;www-data:x:33:33:www-data:/var/www:/usr/sbin/nologin;www-data:x:33:33:www-data:/var/www:/bin/bash;' /etc/passwd
+fi
+
+if [ -n "${SSH_AUTHORIZED_KEY}" ]; then
+    echo "${SSH_AUTHORIZED_KEY}" > /root/.ssh/authorized_keys
+fi
+
 # start all the services
 /usr/local/bin/supervisord -n
 
