15f0d04a8063ddca574c05b9afe45ef0bbdbccc3
Anton Belov <belov_ab@kmplzt.de>
2015-12-16 14:47:44 +0300

    Create run_vnc.sh

---
+++ scripts/run_vnc.sh
@@ -0,0 +1,10 @@
+   #!/bin/bash
+   rm -rf /tmp/.X1*
+   rm -rf /root/.vnc
+   /scripts/start_vnc.sh $VNC_PASSWORD &
+   sleep 5;
+   rm -rf /tmp/.X1*
+   /scripts/start_vnc.sh $VNC_PASSWORD &
+   sleep 5;
+   export DISPLAY=":1"
+   startlxde
