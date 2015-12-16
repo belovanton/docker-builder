6b38bda0dbc5fe810a77256589a481a6dc754bd5
Anton Belov <belov_ab@kmplzt.de>
2015-12-16 10:55:10 +0300

    Update start_vnc.sh

--- scripts/start_vnc.sh
+++ scripts/start_vnc.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/expect
+set pass [lindex $argv 0]
 spawn vncserver :1 -geometry 1280x800 -depth 24 
 expect "assword:"
-send "123q123q\r"
+send "$pass\r"
 expect "erify:"
-send "123q123q\r"
+send "$pass\r"
 expect "ould you like to enter a view-only password"
 send "n\r"
 interact
-
