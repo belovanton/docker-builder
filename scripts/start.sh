dd4bf1b5d154507e62894f02280f5fdcb6a5f8f9
Anton Belov <belov_ab@kmplzt.de>
2015-12-16 10:03:44 +0300

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -13,17 +13,6 @@ if [ -n "${SSH_AUTHORIZED_KEY}" ]; then
     echo "${SSH_AUTHORIZED_KEY}" > /root/.ssh/authorized_keys
 fi
 
-if [ -n "${VNC_PASSWORD}" ]; then
-#!/usr/bin/expect
-spawn vncserver :1 -geometry 1280x800 -depth 24
-expect "assword:"
-send "$VNC_PASSWORD\r"
-expect "erify:"
-send "$VNC_PASSWORD\r"
-expect "ould you like to enter a view-only password"
-send "n\r"
-interact
-fi
 
 # start all the services
 /usr/local/bin/supervisord -n
