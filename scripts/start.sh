37af5fc196cd1a6afd7a6f04592572125b4d46a2
Anton Belov <belov_ab@kmplzt.de>
2015-12-17 10:40:31 +0300

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -10,6 +10,27 @@ fi
 if [ -n "${SSH_AUTHORIZED_KEY}" ]; then
     echo "${SSH_AUTHORIZED_KEY}" > /root/.ssh/authorized_keys
 fi
+if [ -n "${PMA_ENABLE}" ]; then
+echo "" > /etc/phpmyadmin/config-db.php
+    if [ -n "${PMA_DBUSER}" ]; then
+        echo "$dbuser='$PMA_DBUSER'" >> /etc/phpmyadmin/config-db.php
+    fi
+    if [ -n "${PMA_DBPASS}" ]; then
+        echo "$dbpass='$PMA_DBPASS'" >> /etc/phpmyadmin/config-db.php
+    fi
+    if [ -n "${PMA_DBNAME}" ]; then
+        echo "$dbname='$PMA_DBNAME'" >> /etc/phpmyadmin/config-db.php
+    fi
+    if [ -n "${PMA_DBSERVER}" ]; then
+        echo "$dbserver='$PMA_DBSERVER'" >> /etc/phpmyadmin/config-db.php
+    fi
+    if [ -n "${PMA_DBPORT}" ]; then
+        echo "$dbport='$PMA_DBPORT'" >> /etc/phpmyadmin/config-db.php
+    fi
+    if [ -n "${PMA_DBTYPE}" ]; then
+        echo "$dbtype='$PMA_DBTYPE'" >> /etc/phpmyadmin/config-db.php
+    fi
+fi
 
 # start all the services
 /usr/local/bin/supervisord -n
