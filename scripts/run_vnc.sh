d31a95d49c517194edb6886e95d090e960411a34
Anton Belov <belov_ab@kmplzt.de>
2015-12-22 14:34:56 +0300

    Update run_vnc.sh

--- scripts/run_vnc.sh
+++ scripts/run_vnc.sh
@@ -1,6 +1,6 @@
    #!/bin/bash
    rm -rf /tmp/.X1*
    rm -rf /root/.vnc
-   /scripts/start_vnc.sh $VNC_PASSWORD
+   /scripts/start_vnc.sh $VNC_PASSWORD $RESOLUTION
    export DISPLAY=":1"
    startlxde
