9fe2dd2e95b7b8b5c9d5d6078271291ce29d5edc
Anton Belov <belov_ab@kmplzt.de>
2015-12-22 14:34:34 +0300

    Update start_vnc.sh

--- scripts/start_vnc.sh
+++ scripts/start_vnc.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/expect
 set pass [lindex $argv 0]
-spawn vncserver :1 -geometry 1280x800 -depth 24 
+set res [lindex $argv 1]
+spawn vncserver :1 -geometry $res -depth 24 
 expect "assword:"
 send "$pass\r"
 expect "erify:"
